f7699512de3777379c4b331afcf748b199fe1a37
Lisbeth Soria <lisbeth.soria@riosoft369.com>
2024-03-29 00:15:16 -0500

    feat(LoginRepository):La interfaz para encontrar usuario

---
+++ src/main/java/com/example/demo/login/LoginRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.login;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.registro.registro;
+
+@Repository
+public interface LoginRepository extends JpaRepository<registro, Integer>{
+
+	Optional<registro>findRegistroByUsuarioAndContrasenia(String usuario, String contrasenia);
+}
