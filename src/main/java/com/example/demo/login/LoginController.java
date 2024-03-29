c77effeaa1bd5c1494fe7d1b12e642d822513f4f
Lisbeth Soria <lisbeth.soria@riosoft369.com>
2024-03-29 10:11:44 -0500

    Fix(LoginController):import que no se utiliza

--- src/main/java/com/example/demo/login/LoginController.java
+++ src/main/java/com/example/demo/login/LoginController.java
@@ -1,8 +1,5 @@
 package com.example.demo.login;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
